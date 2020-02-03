bc822567935cf3c6f6988420906d5651d62484ea
Yatimisi <yatimisi2012@gmail.com>
2020-02-03 23:09:47 +0800

    feat: First commit.

---
+++ books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    is_online = models.BooleanField()
+
+    def str(self):
+        return self.name
---
+++ books/views.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = []
