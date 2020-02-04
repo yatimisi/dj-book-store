1e627c89bbe1ebbb76b0c81867c72066e646a3eb
Yatimisi <yatimisi2012@gmail.com>
2020-02-04 21:25:20 +0800

    update 2020/02/04-21:25:20

--- books/views.py
+++ books/views.py
@@ -1,10 +1,18 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .models import Book
 from .serializers import BookSerializer
 
 
 class BookViewSet(ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = Book.objects.filter(is_online=True)
     serializer_class = BookSerializer
     permission_classes = []
+
+    @action(['get'], False, 'all', 'all')
+    def all_books(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
--- core/settings.py
+++ core/settings.py
@@ -44,8 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'corsheaders',
+    'django_extensions',
+
     'books',
 ]
 
